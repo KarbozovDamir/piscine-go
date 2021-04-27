a29c67f7dda1e3553f54bee47bf70880db4300b5
Damir K <damirkap89@gmail.com>
2021-04-27 11:18:49 +0600

    changed code

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://01.alem.school/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"damirkap89\"}}){id}}"}' | grep -Eo '[0-9]{1,4}'
+curl -s https://01.alem.school/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"damirkap89\"}}){id}}"}' | jq .data.user | grep .id | cut -d ':' -f2
