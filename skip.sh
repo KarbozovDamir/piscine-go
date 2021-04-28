488cbf938685b024d71a1915495cb4742835e6f1
Damir K <damirkap89@gmail.com>
2021-04-28 15:29:57 +0600

    deleted name of file from code

--- skip.sh
+++ skip.sh
@@ -1,8 +1,2 @@
 #!/bin/bash 
-ls -l | awk 'NR%2==0' >> skip.sh-rwxrwxr-x 1 damir damir     2 Сәу 28 11:38 "\?$*'ChouMi'*$?\"
--rw-rw-r-- 1 damir damir 10240 Сәу 26 20:52 done.tar
--rwxrwxr-x 1 damir damir    47 Сәу 27 19:41 look
--rwxrwxr-x 1 damir damir    23 Сәу 27 14:05 mastertheLS
--rwxrwxr-x 1 damir damir     2 Сәу 27 14:18 r
--rwxrwxr-x 1 damir damir    45 Сәу 28 15:08 skip.sh
--rwxrwxrwx 1 damir damir   108 Сәу 27 13:04 who-are-you.sh
+ls -l | awk 'NR%2==0' 
