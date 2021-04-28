b1305a10f298c4bc586b574fc548d92fba7a7535
Damir K <damirkap89@gmail.com>
2021-04-28 14:59:12 +0600

    changed code

--- skip.sh
+++ skip.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-awk '(NR>1)'
+ls -l | awk '(NR>1)' >> skip.sh-rwxrwxr-x 1 damir damir     2 Сәу 28 11:38 "\?$*'ChouMi'*$?\"
+-rwxrwxr-x 1 damir damir    22 Сәу 28 09:19 countfiles.sh
+-rw-rw-r-- 1 damir damir 10240 Сәу 26 20:52 done.tar
+-rwxrwxr-x 1 damir damir    37 Сәу 26 16:41 hello.sh
+-rwxrwxr-x 1 damir damir    47 Сәу 27 19:41 look
+-rwxrwxr-x 1 damir damir    77 Сәу 28 08:41 lookagain.sh
+-rwxrwxr-x 1 damir damir    23 Сәу 27 14:05 mastertheLS
+-rwxrwxr-x 1 damir damir   135 Сәу 28 00:35 myfamily.sh
+-rwxrwxr-x 1 damir damir     2 Сәу 27 14:18 r
+-rw-rw-r-- 1 damir damir     0 Сәу 26 10:31 README.md
+-rwxrwxr-x 1 damir damir    43 Сәу 28 14:57 skip.sh
+-rwxrwxr-x 1 damir damir   185 Сәу 27 11:16 to-git-or-not-to-git.sh
+-rwxrwxrwx 1 damir damir   108 Сәу 27 13:04 who-are-you.sh
