8b2df79fd76fc5ebfca328a8cf3cca2ea4819e2c
Damir K <damirkap89@gmail.com>
2021-05-03 12:00:36 +0600

    deleted operator plus and num one from for

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,7 +2,7 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	for i := 1; i < nb+1; i++ {
+	for i := 1; i < nb; i++ {
 		result = result * i
 	}
 	if result > 0 {
@@ -12,3 +12,4 @@ func IterativeFactorial(nb int) int {
 		return result
 	}
 }
+
