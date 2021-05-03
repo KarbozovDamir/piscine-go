770da7542134f18379e1fc3a71706ac6b837d138
Damir K <damirkap89@gmail.com>
2021-05-03 13:20:00 +0600

    initial commit

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,14 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb >= 0 && nb < 25 {
-		result := 1
-		for i := 1; i <= nb; i++ {
-			result = result * i
-		}
+	result := 1
+	for i := 1; i < nb+1; i++ {
+		result = result * i
+	}
+	if result > 0 {
 		return result
 	} else {
-		return 0
+		result = 0
+		return result
 	}
 }
