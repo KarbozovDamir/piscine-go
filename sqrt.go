ce6bd06e78666c966307e799379aec9858605036
Damir K <damirkap89@gmail.com>
2021-05-04 08:29:23 +0600

    deleted comments

--- sqrt.go
+++ sqrt.go
@@ -1,8 +1,8 @@
 package piscine
 
-func Sqrt(nb int) int { //nb=16
-	for i := 0; i <= nb; i++ { // i=0 i <=16
-		if i*i == nb { // 0*0 != 16 and then there is an iteration, that is, already i = 1, 0 * 1! = 16 is performed again, and so on as i increases
+func Sqrt(nb int) int { 
+	for i := 0; i <= nb; i++ { 
+		if i*i == nb { 
 			return i
 		}
 	}
