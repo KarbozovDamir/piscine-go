90326e97f2d38cfb006d942acb38a30452874cd6
Damir K <damirkap89@gmail.com>
2021-05-04 06:57:48 +0600

    deleted comment

--- recursivepower.go
+++ recursivepower.go
@@ -4,8 +4,7 @@ func RecursivePower(nb int, power int) int {
 	if power == 0 {
 		return 1
 	}
-
-	if power < 0 { //Negative powers will return 0
+	if power < 0 {
 		return 0
 	}
 	return nb * RecursivePower(nb, power-1)
