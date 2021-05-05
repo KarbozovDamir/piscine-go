cf204945062e5805475f77dc3cc24b856ef1301a
Damir K <damirkap89@gmail.com>
2021-05-05 15:06:08 +0600

    changed condition

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(s string) int {
 	count := 0
 	for _, a := range s {
-		if (a >= 'A' && a >= 'Z') || (a >= 'a' && a >= 'z') {
+		if (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') {
 			count++
 		}
 	}
