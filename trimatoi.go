9e9adcda939d2ad1152d0a12a6c8b4f7a4a36346
Damir K <damirkap89@gmail.com>
2021-05-07 05:58:28 +0600

    changed code

--- trimatoi.go
+++ trimatoi.go
@@ -1,14 +1,14 @@
 package piscine
 
-func TrimAtoi(s string) int {
-	res := 0
-	help := 1
-	for _, i := range s {
-		if i >= 48 && i <= 57 {
-			res = (int(i) - 48) + res*10
-		} else if i == '-' || res == 0 {
-			help = -1
+func TrimAtoi(str string) int {
+	var arr string
+	for _, item := range str {
+		if item == '-' && StrLen(arr) == 0 {
+			arr = string(item) + arr
+		}
+		if item >= 48 && item <= 57 {
+			arr = arr + string(item)
 		}
 	}
-	return res * help
+	return Atoi(arr)
 }
