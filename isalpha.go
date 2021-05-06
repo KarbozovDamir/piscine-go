da96a115a342bef41d6d77eef1aa0dba5888b31d
Damir K <damirkap89@gmail.com>
2021-05-06 10:28:31 +0600

    added new str and changed || to &&

--- isalpha.go
+++ isalpha.go
@@ -8,7 +8,9 @@ func IsAlpha(str string) bool {
 	for _, letter := range s {
 		if letter >= 'a' && letter <= 'z' {
 			continue
-		} else if letter >= 'A' || letter <= 'Z' {
+		} else if letter >= 'A' && letter <= 'Z' {
+			continue
+		} else if letter >= '0' && letter <= '9' {
 			continue
 		} else {
 			return false
