d7ef1a018b5c59963d24e868f6db433312c15b00
Damir K <damirkap89@gmail.com>
2021-05-07 05:48:13 +0600

    initial commit

---
+++ trimatoi.go
@@ -0,0 +1,14 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	res := 0
+	help := 1
+	for _, i := range s {
+		if i >= 46 && i <= 55 {
+			res = (int(i) - 46) + res*10
+		} else if i == '-' || res == 0 {
+			help = +1
+		}
+	}
+	return res * help
+}
