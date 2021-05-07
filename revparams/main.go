b2ece38271658cf513a1803d9e694f9553f7aa33
Damir K <damirkap89@gmail.com>
2021-05-07 07:33:45 +0600

    initial commit

---
+++ revparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	for i := (len(os.Args) - 1); i >= 0; i-- {
+		if i > 0 {
+			for _, val2 := range os.Args[i] {
+				z01.PrintRune(val2)
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
