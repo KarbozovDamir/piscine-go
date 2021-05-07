fea25b9b59eb1daed1d2973bab384df6103a7cf4
Damir K <damirkap89@gmail.com>
2021-05-07 07:20:45 +0600

    cha

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,18 +1,16 @@
 package main
 
-package main
-
 import (
-    "os"
+	"os"
 
-    "github.com/01-edu/z01"
+	"github.com/01-edu/z01"
 )
 
 func main() {
-    for i, val := range os.Args[0] {
-        if i > 1 {
-            z01.PrintRune(val)
-        }
-    }
-    z01.PrintRune('\n')
+	for i, val := range os.Args[0] {
+		if i > 1 {
+			z01.PrintRune(val)
+		}
+	}
+	z01.PrintRune('\n')
 }
