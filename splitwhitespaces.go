a73f30c680b9f9445de452c4151d680d3271b4a0
Damir K <damirkap89@gmail.com>
2021-05-09 05:21:33 +0600

    initial commit

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,24 +1,48 @@
-package main
+package piscine
 
-import (
-	"os"
-
-	"github.com/01-edu/z01"
-)
-
-func main() {
-	a := os.Args
-	b := 0
-
-	for range a {
-		b++
+func SplitWhiteSpaces(str string) []string {
+	spaces := 0
+	totalLength := 0
+	for range str {
+		totalLength++
 	}
-
-	for i := 1; i < b; i++ {
-		for _, el := range a[i] {
-			z01.PrintRune(el)
+	for _, item := range str {
+		if item == ' ' {
+			spaces++
+		}
+	}
+	whiteSpaces := make([]int, spaces+2)
+	i := 0
+	for index, item := range str {
+		if index == 0 {
+			whiteSpaces[i] = index
+			i++
+		}
+		if item == ' ' || item == '\n' {
+			whiteSpaces[i] = index
+			i++
+		}
+	}
+	whiteSpaces[i] = totalLength
+	lengthOfArray := 0
+	for range whiteSpaces {
+		lengthOfArray++
+	}
+	arr := make([]string, spaces+1)
+	for j := 0; j < lengthOfArray-1; j++ {
+		if j == 0 {
+			for k := whiteSpaces[j]; k < whiteSpaces[j+1]; k++ {
+				arr[j] = arr[j] + string(str[k])
+			}
+		} else {
+			if whiteSpaces[j+1]-whiteSpaces[j] != 1 {
+				for k := whiteSpaces[j] + 1; k < whiteSpaces[j+1]; k++ {
+					arr[j] = arr[j] + string(str[k])
+				}
+			}
 		}
 
-		z01.PrintRune('\n')
 	}
+
+	return arr
 }
