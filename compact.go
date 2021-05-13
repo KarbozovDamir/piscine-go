8bd8c328c610e24022ef1dee3a0323f1bdf61383
Damir K <damirkap89@gmail.com>
2021-05-13 22:12:47 +0600

    initial commit

--- compact.go
+++ compact.go
@@ -3,14 +3,14 @@ package piscine
 func Compact(ptr *[]string) int {
 	length := 0
 	for _, s := range *ptr {
-		if s != "" {
+		if s != " " {
 			length++
 		}
 	}
 	count := 0
 	array := make([]string, length)
 	for _, s := range *ptr {
-		if s != "" {
+		if s != " " {
 			array[count] = s
 			count++
 		}
