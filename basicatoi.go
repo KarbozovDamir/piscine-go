f27f71ff16928068f9e0c09c9e33c73013cf0c74
damirkap89 <damirkap89@gmail.com>
2021-07-28 00:56:03 +0600

    deleted

--- basicatoi.go
+++ basicatoi.go
@@ -1,5 +1,6 @@
 package piscine
 
+// BasicAtoi : ba
 func BasicAtoi(s string) int {
 	res := 0
 	for _, el := range s {
--- test/main.go
+++ test/main.go
@@ -9,36 +9,4 @@ func main() {
 	fmt.Println(piscine.BasicAtoi("12345"))
 	fmt.Println(piscine.BasicAtoi("0000000012345"))
 	fmt.Println(piscine.BasicAtoi("000000"))
-
-	// s := "Hello World!"
-	// s = piscine.StrRev(s)
-	// fmt.Println(s)
-
-	// piscine.PrintCombN(1)
-	// piscine.PrintCombN(3)
-	// piscine.PrintCombN(9)
-
-	// s := "Hello World!"
-	// s = piscine.StrRev(s)
-	// fmt.Println(s)
-
 }
-
-// 	x := []int{}
-// 	min := x[0]
-// 	for _, el := range x {
-// 		if el < min {
-// 			min = el
-// 		}
-// 	}
-// 	fmt.Println(min)
-// }
-
-//
-// func main() {
-// 	for i := 0; i < 5; i++ {
-// 		for j := 5 - i; j > 0; j-- {
-// 			fmt.Print("*")
-// 		}
-// }
-// }
