293281244a0601d42f28f472116a430dad94f401
damirkap89 <damirkap89@gmail.com>
2021-07-28 03:43:14 +0600

    added comment

--- basicatoi2.go
+++ basicatoi2.go
@@ -2,14 +2,14 @@ package piscine
 
 // BasicAtoi2 : ba2
 func BasicAtoi2(s string) int {
-	res := 0
-	if isNumeric(s) {
-		for _, el := range s {
+	if isNumeric(s) { // если нашли число
+		res := 0
+		for _, el := range s { // собираем число в переменной - res
 			res = res*10 + int(el-48)
 		}
 		return res
 	}
-	return 0
+	return 0 // если не число тогда возвращаем ноль
 }
 
 func isNumeric(s string) bool {
