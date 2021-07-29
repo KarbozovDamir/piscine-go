3c12da97cfdbeeee7df2d0cbad05ee0642a5ae26
damirkap89 <damirkap89@gmail.com>
2021-07-29 16:48:09 +0600

    initial commit

--- test/main.go
+++ test/main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"piscine"
+
+	"github.com/01-edu/z01"
+)
+
 func main() {
+	piscine.PrintNbr(-123)
+	piscine.PrintNbr(0)
+	piscine.PrintNbr(123)
+	z01.PrintRune('\n')
 	// fmt.Println(piscine.BasicAtoi2("12345"))
 	// fmt.Println(piscine.BasicAtoi2("0000000012345"))
 	// fmt.Println(piscine.BasicAtoi2("012 345"))
